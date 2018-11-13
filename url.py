b4d1929dcd41df299a5e4b798ba8a006b94ac810
Ubuntu <ubuntu@ip-172-31-23-24.ap-northeast-2.compute.internal>
2018-11-13 04:41:44 +0000

    1000개 단위..

--- url.py
+++ url.py
@@ -9,7 +9,7 @@ for line1 in lines1:
         f2 = open('url/url'+str(cnt1)+'.txt', 'a')
     f2.write(line1)
     cnt2+=1
-    if cnt2 == 20:
+    if cnt2 == 1000:
         cnt1+=1
         cnt2=1
     f2.close()
